09fdc527f302603cf153feda27505894e8b2511f
vildan1 <vildanakbulut98@gmail.com>
2023-04-14 23:51:36 +0300

    code review

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
-import { ExampleComponent } from 'va-button-ui'
+import { Button } from 'va-button-ui'
 import 'va-button-ui/dist/index.css'
-
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <div className='main'>
+    <Button type='primary' text="Click Me" onClick = {() => alert("PRIMARY")} /> <br />
+    <Button type='danger' text="Click Me" onClick = {() => alert("DANGER")} /> <br />
+    <Button type='dashed' text="Click Me" onClick = {() => alert("DASHED")} /> <br />
+    <Button type='warning' text="Click Me" onClick = {() => alert("WARNING")} /> <br />
+
+    </div>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+//button component
+export const Button = ({type, onClick}) => {
+    const upLetter = (str)=>{
+      return str.charAt(0).toUpperCase() + str.slice(1)
+    }
+    
+  return (  
+    <div className={styles.main}>
+    <button onClick={onClick} className={`${styles[type]} ${styles.button}`}>
+      {upLetter(type)} Button
+    </button>
+    </div>
+  )
 }
